8093bee391722acf2734c30ac34f1a7dffd890c9
joaomlg <joaomarcos1999jm@gmail.com>
2022-05-15 13:51:22 -0300

    Physical and simulated implementation file created

---
+++ integration/SimulatedCraneController.py
@@ -0,0 +1,26 @@
+from . import ICraneController
+
+class SimulatedCraneController(ICraneController):
+  def reset_crane() -> None:
+    raise NotImplementedError
+
+  def rotate_spear(degrees: float) -> None:
+    raise NotImplementedError
+
+  def move_appliance(height: float) -> None:
+    raise NotImplementedError
+
+  def toggle_electromagnet(state: bool) -> None:
+    raise NotImplementedError
+
+  def get_spear_angle() -> float:
+    raise NotImplementedError
+  
+  def get_appliance_height() -> float:
+    raise NotImplementedError
+
+  def get_electromagnet_state() -> bool:
+    raise NotImplementedError
+  
+  def get_ultrasonic_distance() -> float:
+    raise NotImplementedError
