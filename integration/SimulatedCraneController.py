3da73d4985bcf7cbb1d0bb6085bee75a88b5633b
Gabriel Lopes Machado <gabriel.galoma@gmail.com>
2022-07-05 21:54:36 -0300

    add: simulation behavior completed

--- integration/SimulatedCraneController.py
+++ integration/SimulatedCraneController.py
@@ -1,23 +1,26 @@
 import math
-from integration.ICraneController import ICraneController
-from utils.SingletonMetaClass import AbstractSingletonMetaClass
-import math
+# from integration.ICraneController import ICraneController
+from ICraneController import ICraneController # TO remove
+import zmqRemoteApi as zmqRemoteApi # TO remove
 import time
 
 VELOCITY_SPEAR = .25
-VELOCITY_APPLIANCE = .15
+VELOCITY_APPLIANCE = .015
+SIMULATION_DISTANCE = 42
+REAL_DISTANCE = 27
 
-class SimulatedCraneController(ICraneController, metaclass=AbstractSingletonMetaClass):
+class SimulatedCraneController(ICraneController):
   def __init__(self):
     try : 
-      import integration.zmqRemoteApi as zmqRemoteApi
+      # import integration.zmqRemoteApi as zmqRemoteApi
       self.sim = zmqRemoteApi.RemoteAPIClient().getObject('sim')
       print("Connected to simulation")
     except :
       print("Could not connect to the simulation.")
       exit()
-    self.spear = self.sim.getObject('/Arm_actuator')
-    self.app = self.sim.getObject('/Hoist_actuator')
+    self.spear = self.sim.getObject('/armActuator')
+    self.app = self.sim.getObject('/hoistActuator')
+    self.sensor = self.sim.getObject("/distanceSensor")
     
     self.sim.startSimulation()
     self.spear_angle = .0
@@ -28,34 +31,51 @@ class SimulatedCraneController(ICraneController, metaclass=AbstractSingletonMeta
     self.sim.startSimulation()
     self.spear_angle = .0
     self.appliance_height = .0
+    self.eletromagnatic_state = False
 
   def rotate_spear(self, degrees: float) -> None:
     multiplier = 1
     if degrees < 0:
       multiplier = -1
 
-    sleep_time = degrees/(VELOCITY_SPEAR * (180/math.pi))
+    radians = degrees/180 * math.pi
+    position = self.sim.getJointPosition(self.spear)
+    target_radians = position + radians
     self.sim.setJointTargetVelocity(self.spear, multiplier * VELOCITY_SPEAR)
-    time.sleep(sleep_time)
-    self.sim.setJointTargetVelocity(self.spear, 0)
     
-    self.spear_angle += degrees
+    while True:
+      position = self.sim.getJointPosition(self.spear)
+      if (multiplier == 1 and position > target_radians) or (multiplier == -1 and position < target_radians):
+        break
+
+    self.sim.setJointTargetVelocity(self.spear, 0)
 
   def move_appliance(self, height: float) -> None:
-    # TODO: Calc appliance limits and filter
-    multiplier = 1
-    if height < 0:
+    multiplier = 0
+    height = height/100 * SIMULATION_DISTANCE/REAL_DISTANCE
+    position = -1 * self.sim.getJointPosition(self.app)
+    target_position = position + height*-1
+    if height > 0:
+      multiplier = 1
+    else:
       multiplier = -1
 
-    sleep_time = height * VELOCITY_APPLIANCE
-    self.sim.setJointTargetVelocity(self.spear, multiplier * VELOCITY_APPLIANCE)
-    time.sleep(sleep_time)
-    self.sim.setJointTargetVelocity(self.spear, 0)
+    self.sim.setJointTargetVelocity(self.app, multiplier * VELOCITY_APPLIANCE)
 
-    self.height += height
+    while True:
+      position = -1 * self.sim.getJointPosition(self.app)
+      if (multiplier == -1 and position > target_position) or (multiplier == 1 and position < target_position):
+        break
+
+    self.sim.setJointTargetVelocity(self.app, 0)
+
+    position = -1 * self.sim.getJointPosition(self.app)
+
+    self.appliance_height = position * 100 * REAL_DISTANCE/SIMULATION_DISTANCE
 
   def toggle_electromagnet(self, state: bool) -> None:
-    raise NotImplementedError
+    self.eletromagnatic_state = state
+    self.sim.pushUserEvent("toggleEletromagnet", -1, -1, {"state": state})
 
   def get_spear_angle(self) -> float:
     return self.spear_angle
@@ -64,7 +84,44 @@ class SimulatedCraneController(ICraneController, metaclass=AbstractSingletonMeta
     return self.appliance_height
 
   def get_electromagnet_state(self) -> bool:
-    raise NotImplementedError
+    return self.eletromagnatic_state
   
   def get_ultrasonic_distance(self) -> float:
-    raise NotImplementedError
+    data = self.sim.readProximitySensor(self.sensor)
+    return data[1]
+
+if __name__ == "__main__":
+  pass
+  # simulatedCrane = SimulatedCraneController()
+  # Active eletromagnetic
+  # time.sleep(1)
+  # simulatedCrane.toggle_electromagnet(True)
+  # time.sleep(1)
+  # simulatedCrane.toggle_electromagnet(False)
+  # time.sleep(1)
+  # simulatedCrane.toggle_electromagnet(True)
+
+  # Move Applience
+  # simulatedCrane.move_appliance(-10)
+  # time.sleep(1)
+  # simulatedCrane.move_appliance(5)
+  # time.sleep(1)
+  # simulatedCrane.move_appliance(-20)
+
+  # Rotate Spear
+  # simulatedCrane.rotate_spear(90)
+  # time.sleep(1)
+  # simulatedCrane.rotate_spear(-45)
+  # time.sleep(1)
+  # simulatedCrane.rotate_spear(20)
+  # time.sleep(1)
+  # simulatedCrane.rotate_spear(270)
+
+  # simulatedCrane.toggle_electromagnet(True)
+  # simulatedCrane.move_appliance(-23.2)
+  # time.sleep(1)
+  # simulatedCrane.move_appliance(23)
+  # simulatedCrane.rotate_spear(90)
+  # simulatedCrane.toggle_electromagnet(False)
+
+
